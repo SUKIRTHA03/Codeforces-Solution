1b2e3a7e5272e0b53871898d5aaa700ec04ed7fe
Sukirtha <sukirthaedu@gmail.com>
2026-09-12 11:09:16 +0530

    Complete B Swap and Delete solution

--- BSwapAndDelete.java
+++ BSwapAndDelete.java
@@ -1,3 +1,40 @@
+import java.util.*;
 public class BSwapAndDelete {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            String s = sc.next();
+
+            int zero = 0;
+            int one = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '0')
+                    zero++;
+                else
+                    one++;
+            }
+
+            int ans = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '0') {
+                    if (one > 0)
+                        one--;
+                    else
+                        break;
+                } else {
+                    if (zero > 0)
+                        zero--;
+                    else
+                        break;
+                }
+            }
+
+            System.out.println(zero + one);
+        }
+    }
 }
